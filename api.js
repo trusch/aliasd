8d2e01f87f796d6bb56c28691373714dbfc13e97
Tino Rusch <tino.rusch@gmail.com>
2017-10-25 13:53:33 +0200

    added caching layer;

--- api.js
+++ api.js
@@ -11,8 +11,7 @@
  * set(scope, id, val) => set a alias
  * del(scope, id)      => delete a alias
  */
-
-class AliasAPI {
+export class AliasAPI {
   constructor(endpoint){
     this.endpoint = endpoint;
   }
@@ -82,4 +81,87 @@ class AliasAPI {
       return resp.text();
     });
   }
+}
+
+
+/**
+ * JS class to interact with aliasd and cache the results
+ * ================================
+ *
+ * Methods:
+ * ========
+ *
+ * initScopes(scopes)  => initialize the given scopes (maps to getAll)
+ * get(scope, id)      => returns the alias
+ * create(scope, id)   => create a new, random alias
+ * set(scope, id, val) => set a alias
+ * del(scope, id)      => delete a alias
+ */
+export class AliasCache extends AliasAPI {
+  constructor(url){
+    super(url);
+    this.scopes = {};
+  }
+
+  initScopes(scopes) {
+    const promises = [];
+    if (scopes !== undefined && scopes.length > 0) {
+      for(let i=0; i<scopes.length; i++) {
+        promises.push(this.getAll(scopes[i]));
+      }
+    }
+    return Promise.all(promises).then((results) => {
+      for(let i=0; i<scopes.length; i++) {
+        const scope = scopes[i];
+        this.scopes[scope] = results[i];
+      }
+      return this;
+    });
+  }
+
+  get(scope, id) {
+    return new Promise((resolve,reject)=>{
+      const cache = this.scopes[scope] || {};
+      if (cache[id] === undefined) {
+        return super.get(scope, id)
+        .then((alias)=>{
+          resolve(alias);
+        })
+        .catch(reject);
+      }else {
+        resolve(cache[id]);
+      }
+    });
+  }
+
+  create(scope, id) {
+    return super.create(scope, id)
+    .then((alias) => {
+      const cache = this.scopes[scope] || {};
+      cache[id] = alias;
+      this.scopes[scope] = cache;
+      return alias;
+    });
+  }
+
+  del(scope, id) {
+    return super.del(scope, id)
+    .then(() => {
+      const cache = this.scopes[scope] || {};
+      delete cache[id];
+      this.scopes[scope] = cache;
+      return true;
+    });
+  }
+
+  set(scope, id, alias) {
+    return super.set(scope, id, alias)
+    .then(() => {
+      const cache = this.scopes[scope] || {};
+      cache[id] = alias;
+      this.scopes[scope] = cache;
+      return alias;
+    });
+  }
+
 }
